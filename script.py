7167a9b91dd01184caff281186af5abd24dce616
Suhail Ahmed <suhailahmedbittububby@gmail.com>
2021-04-28 10:13:46 +0530

    Text+videos sending func

--- script.py
+++ script.py
@@ -9,22 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 import pandas
 import time
 
-# Load the chrome driver
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
-count = 0
-
-# Open WhatsApp URL in chrome browser
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 20)
-
-# Read data from excel
-excel_data = pandas.read_excel('Customer.xlsx', sheet_name='Customers')
-
-# Iterate excel rows till to finish
-for column in excel_data['Name'].tolist():
-    # Assign customized message
-    message = excel_data['Message'][0]
-    car = excel_data['Car'][count]
+def sendmsg(contact,message):
     # Locate search box through x_path
     search_box = '//*[@id="side"]/div[1]/div/label/div/div[2]'
     person_title = wait.until(lambda driver:driver.find_element_by_xpath(search_box))
@@ -33,9 +18,8 @@ for column in excel_data['Name'].tolist():
     person_title.clear()
 
     # Send contact number in search box
-    person_title.send_keys(str(excel_data['Contact'][count]))
-    count = count + 1
-
+    person_title.send_keys(contact)
+    
     # Wait for 2 seconds to search contact number
     time.sleep(2)
 
@@ -50,9 +34,91 @@ for column in excel_data['Name'].tolist():
         actions = ActionChains(driver)
         actions.send_keys(message)
         actions.send_keys(Keys.ENTER)
+        time.sleep(0.5)
         print("Message succesfully sent to ", column)
         print(count, " messages sent.")
         actions.perform()
+
+def sendvid(contact, path):
+    # Locate search box through x_path
+    search_box = '//*[@id="side"]/div[1]/div/label/div/div[2]'
+    person_title = wait.until(lambda driver:driver.find_element_by_xpath(search_box))
+
+    # Clear search box if any contact number is written in it
+    person_title.clear()
+
+    # Send contact number in search box
+    person_title.send_keys(contact)
+    
+    # Wait for 2 seconds to search contact number
+    time.sleep(2)
+
+    try:
+        # Load error message in case unavailability of contact number
+        element = driver.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/span')
+    except NoSuchElementException:
+        # Format the message from excel sheet
+        person_title.send_keys(Keys.ENTER)
+        # Find and click the attach button
+        attachment_box = driver.find_element_by_xpath('//div[@title = "Attach"]')
+        attachment_box.click()
+
+        #Find and click the video option
+        image_box = driver.find_element_by_xpath(
+            '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+        image_box.send_keys(path)
+
+        # Wait until the video loads
+        time.sleep(3)
+
+        # Hit the send button
+        send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
+        send_button.click()
+        print("Message succesfully sent to ", column)
+        print(count, " messages sent.")
+    
+# Load the chrome driver
+driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+count = 0
+
+# Open WhatsApp URL in chrome browser
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 20)
+
+# Read data from excel
+excel_data = pandas.read_excel('Customer.xlsx', sheet_name='Customers')
+
+# User choses between sending a text message or a video message
+msgtyp = input("Enter the kind of message you want to send(v/t): ")
+
+if msgtyp == 't':
+    # Iterate excel rows till to finish
+    for column in excel_data['Name'].tolist():
+        #Stop once it reaches first nan value
+        if excel_data['Name'].isnull()[count]:
+                print("Job completed.")
+                break
+        # Assign customized message
+        message = excel_data['Message'][0]
+        car = excel_data['Car'][count]
+        contact = str(int(excel_data['Contact'][count]))
+        sendmsg(contact,message)
+        count = count + 1
+
+if msgtyp == 'v':
+    # Take input for path to video
+    path = input("Enter complete path to the video: ")
+
+    # Iterate excel rows till to finish
+    for column in excel_data['Name'].tolist():
+        #Stop once it reaches first nan value
+        if excel_data['Name'].isnull()[count]:
+                print("Job completed.")
+                break
+        # Assign customized message
+        contact = str(int(excel_data['Contact'][count]))
+        sendvid(contact,path)
+        count = count + 1
 
 # Close chrome browser
 driver.quit()
